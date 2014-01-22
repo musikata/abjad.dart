dbfa482e8b35681ea9285cfa079ee2b4fa78659f
adorsk <adorsk@gmail.com>
2014-01-21 20:42:10 -0500

    initial start...testing stub

---
+++ lib/abc/AbjadObject.dart
@@ -0,0 +1,10 @@
+library AbjadObject;
+
+
+/** 
+ * Abstract base class from which all custom classes should inherit.
+ * Abjad objects compare equal only with equal object IDs.
+ */
+class AbjadObject {
+}
+
---
+++ test/abjad/abc/AbjadObject_test.dart
@@ -0,0 +1,9 @@
+import 'package:unittest/unittest.dart';
+
+import 'package:abjad/abc/AbjadObject.dart';
+
+main() {
+  test('AbjadObject exists', (){
+    expect(AbjadObject, isNotNull);
+  });
+}
